b5005c7b647c67bdeaea061379e192f8336f3385
“Giulia <giuliaketlin98@gmail.com>
2024-04-10 20:57:01 -0300

    fisrt commit

---
+++ Idade de Cachorro/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Idade de Cachorro
+//
+//  Created by Luccas Santana Marinho on 03/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var campoIdadedeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(campoIdadedeCachorro.text!)! * 7
+        legendaResultado.text =  "A idade do cachorro é:" + String (idade)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
